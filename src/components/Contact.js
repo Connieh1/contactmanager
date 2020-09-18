de03f7c7aad007fd8c926a33755a08253887c984
github username <constance.hyman@mchsi.com>
2020-09-18 16:30:10 -0700

    Add delete functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,10 @@ class Contact extends Component {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   };
 
+  onDeleteClick = (e) => {
+    this.props.deleteClickHandler();
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
@@ -17,7 +21,17 @@ class Contact extends Component {
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i onClick={this.onShowClick} className="fa fa-sort-down" />
+          {name}{" "}
+          <i
+            onClick={this.onShowClick}
+            className="fa fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            className="fa fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onDeleteClick}
+          ></i>
         </h4>
         {showContactInfo ? (
           <ul className="list-group">
@@ -32,6 +46,7 @@ class Contact extends Component {
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired,
 };
 
 export default Contact;
